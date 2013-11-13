7f4abe5d5347a7e2ad43b8fbb5b9da14fff9c64c
yunai39 <yunai39@gmail.com>
2013-11-13 08:49:33 +0100

    Change a few thing for the redirection based on the user Role

--- Handler/AuthenticationSuccessHandler.php
+++ Handler/AuthenticationSuccessHandler.php
@@ -7,18 +7,41 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\DefaultAuthenticationSuccessHandler;
 use Symfony\Component\Security\Http\HttpUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AuthenticationSuccessHandler extends DefaultAuthenticationSuccessHandler {
+	/**
+     * Router
+     *
+     * @var Router
+     */
+    protected $router;
 
-    public function __construct( HttpUtils $httpUtils, array $options ) {
+    /**
+     * Routes
+     *
+     * @var array
+     */
+    protected $routes = array();
+	
+    public function __construct( HttpUtils $httpUtils, array $options, $router , $routes ) {
         parent::__construct( $httpUtils, $options );
+        $this->router = $router;
+        $this->routes = $routes;
     }
 
     public function onAuthenticationSuccess( Request $request, TokenInterface $token ) {
         if( $request->isXmlHttpRequest() ) {
             $response = new JsonResponse( 'true' );
         } else {
-            $response = parent::onAuthenticationSuccess( $request, $token );
+            $user = $token->getUser();
+	        $roles = $user->getRoles();
+	        $role = reset($roles);
+			$referer_url = $request->headers->get('referer');
+	        if (array_key_exists($role, $this->routes)) {
+	        	$referer_url = $this->routes[$role];
+	        }
+	        return new RedirectResponse($this->router->generate($referer_url));
         }
         return $response;
     }
